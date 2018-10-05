d9633fa0254aff78e0e67ec8e28c66edc966393c
Paulo D Vasconcelos <paulorcv@MacBook-Pro-de-Paulo.local>
2018-10-05 17:59:40 -0300

    codigo inicial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,73 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MessageList from './MessageList';
+import Title from './Title';
+import SendMessageForm from './SendMessageForm';
+import Chatkit from '@pusher/chatkit';
 
-class App extends Component {
+
+const instanceLocator = "v1:us1:d4824d95-d516-4aca-8f9f-43bb6f10dc82";
+const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/d4824d95-d516-4aca-8f9f-43bb6f10dc82/token";
+const username = "paulorcv";
+const roomId = 17909154;
+
+
+class App extends React.Component {
+  constructor() {
+      super()
+      this.state = {
+          messages: []
+      }
+      this.sendMessage = this.sendMessage.bind(this)
+  } 
+  
+  componentDidMount() { 
+      const chatManager = new Chatkit.ChatManager({
+          instanceLocator: instanceLocator,
+          userId: username,
+          tokenProvider: new Chatkit.TokenProvider({
+              url: testToken
+          })
+      })
+      
+      chatManager.connect()
+      .then(currentUser => {
+          this.currentUser = currentUser
+          this.currentUser.subscribeToRoom({
+          roomId: roomId,
+          hooks: {
+              onNewMessage: message => {
+
+                  this.setState({
+                      messages: [...this.state.messages, message]
+                  })
+              }
+          }
+      })
+    })
+  }
+  
+  sendMessage(text) {
+      this.currentUser.sendMessage({
+          text,
+          roomId: roomId
+      })
+  }
+  
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      return (
+          <div className="app">
+            <Title />
+            <MessageList 
+                roomId={this.state.roomId}
+                messages={this.state.messages} />
+            <SendMessageForm
+                sendMessage={this.sendMessage} />
+          </div>
+      );
   }
 }
+
 
 export default App;
---
+++ src/Title.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+function Title() {
+    return <p className="title">My awesome chat app</p>
+  }
+
+  export default Title;
